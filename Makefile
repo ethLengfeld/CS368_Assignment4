30f8853eb0988a26777683ff771c83fdaa2cac16
ethLengfeld <elengfeld@wisc.edu>
2020-03-09 22:52:12 -0500

    added all files with header

---
+++ Makefile
@@ -0,0 +1,21 @@
+# Ethan Lengfeld
+# 9074020836
+# Makefile to compile Assignment 4 - Department Database code
+
+CXX=g++
+CXXFLAGS = -Wall
+EXE = DemoDepartmentDatabase
+
+#all: demo.o SummarizeGrades.o
+#	$(CXX) $(CXXFLAGS) -o $(EXE) demo.o SummarizeGrades.o
+
+#SummarizeGrades.o: SummarizeGrades.cpp SummarizeGrades.h
+#	$(CXX) $(CXXFLAGS) -c SummarizeGrades.cpp
+
+#demo.o: demo.cpp SummarizeGrades.h
+#	$(CXX) $(CXXFLAGS) -c demo.cpp
+
+#clean:
+#	-rm demo.o
+#	-rm SummarizeGrades.o
+#	-rm DemoDepartmentDatabase
---
+++ demo.cpp
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File: DisplayUtility.cpp, demo.cpp
+// This File: demo.cpp
+// Other Files: 	 Person.h / Person.cpp / Employee.cpp / Employee.h / 
+//                   Student.cpp / Student.h / Faculty.cpp / Faculty.h /
+//                   TeachingAssistant.cpp / TeachingAssistant.h / 
+//                   DisplayUtility.h / DisplayUtility.cpp / demo.cpp / 
+//                   Makefile / README.md
+// Semester:         CS 368 Spring 2020
+//
+// Author:           Ethan Lengfeld
+// Email:            elengfeld@wisc.edu
+// CS Login:         lengfeld
+//
+//
+// Online sources:   
+//
+////////////////////////////////////////////////////////////////////////////////
