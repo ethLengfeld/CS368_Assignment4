ae2be06e38550e36ce6791e0b80e26995fa10492
ethLengfeld <elengfeld@wisc.edu>
2020-03-10 10:21:59 -0500

    Person constructor

--- Person.h
+++ Person.h
@@ -13,6 +13,75 @@
 // CS Login:         lengfeld
 //
 //
-// Online sources:   
+// Online sources:   https://stackoverflow.com/questions/187640/default-parameters-with-c-constructors
 //
 ////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef PERSON
+#define PERSON
+
+/**
+ * TODO
+ */
+
+#include <string>
+#include <vector>
+
+class Person {
+
+    public:
+
+        // TODO add comment
+        enum classType {
+            PER,
+            EMP,
+            STU,
+            FAC,
+            TA
+        };
+
+        /**
+         * TODO
+         */
+        std::string name;
+        int ID;
+        classType type;
+        vector<int>* courseId;
+
+        /**
+         * TODO
+         */
+        Person(std::string name = "Unknown peson", int ID = -1, vector<int>* courseId = NULL, classType type = PER) : 
+            name(name), 
+            ID(ID),
+            type(type),
+            courseId(courseId)
+        {}
+
+        /**
+         * TODO
+         */
+        void displayDetails();
+
+        /**
+         * TODO
+         * @return vector <int>* - 
+         */
+        vector <int>* getCourseId();
+
+        /**
+         * TODO
+         * @return classType -
+         */
+        classType getClassType();
+
+        /**
+         * TODO
+         * @return std::string - 
+         */
+        std::string getName();
+
+};
+
+#endif
